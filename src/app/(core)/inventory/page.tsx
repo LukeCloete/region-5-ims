afda77da6c16424cd05276ba6d61314ecbde650d
Luke Cloete <Cloeteluke@gmail.com>
2025-08-18 20:55:39 +0200

    add inventory page and related components

--- src/app/(core)/inventory/page.tsx
+++ src/app/(core)/inventory/page.tsx
@@ -15,14 +15,129 @@ async function getData(): Promise<Item[]> {
       description: "Description for Item 1",
       dateOfPurchase: new Date(),
     },
-    // ...
+    {
+      id: "728ed52g",
+      barcode: 654321,
+      serialNumber: "SN654321",
+      cluster: "B",
+      name: "Item 2",
+      quantity: 50,
+      categoryId: "cat2",
+      description: "Description for Item 2",
+      dateOfPurchase: new Date(),
+    },
+    // Add more items as needed
+    {
+      id: "728ed52h",
+      barcode: 789012,
+      serialNumber: "SN789012",
+      cluster: "C",
+      name: "Item 3",
+      quantity: 75,
+      categoryId: "cat3",
+      description: "Description for Item 3",
+      dateOfPurchase: new Date(),
+    },
+    {
+      id: "728ed52f",
+      barcode: 123456,
+      serialNumber: "SN123456",
+      cluster: "A",
+      name: "Item 1",
+      quantity: 100,
+      categoryId: "cat1",
+      description: "Description for Item 1",
+      dateOfPurchase: new Date(),
+    },
+    {
+      id: "728ed52g",
+      barcode: 654321,
+      serialNumber: "SN654321",
+      cluster: "B",
+      name: "Item 2",
+      quantity: 50,
+      categoryId: "cat2",
+      description: "Description for Item 2",
+      dateOfPurchase: new Date(),
+    },
+    // Add more items as needed
+    {
+      id: "728ed52h",
+      barcode: 789012,
+      serialNumber: "SN789012",
+      cluster: "C",
+      name: "Item 3",
+      quantity: 75,
+      categoryId: "cat3",
+      description: "Description for Item 3",
+      dateOfPurchase: new Date(),
+    },
+    {
+      id: "728ed52f",
+      barcode: 123456,
+      serialNumber: "SN123456",
+      cluster: "A",
+      name: "Item 1",
+      quantity: 100,
+      categoryId: "cat1",
+      description: "Description for Item 1",
+      dateOfPurchase: new Date(),
+    },
+    {
+      id: "728ed52g",
+      barcode: 654321,
+      serialNumber: "SN654321",
+      cluster: "B",
+      name: "Item 2",
+      quantity: 50,
+      categoryId: "cat2",
+      description: "Description for Item 2",
+      dateOfPurchase: new Date(),
+    },
+    // Add more items as needed
+    {
+      id: "728ed52h",
+      barcode: 789012,
+      serialNumber: "SN789012",
+      cluster: "C",
+      name: "Item 3",
+      quantity: 75,
+      categoryId: "cat3",
+      description: "Description for Item 3",
+      dateOfPurchase: new Date(),
+    },
+    {
+      id: "728ed52h",
+      barcode: 789012,
+      serialNumber: "SN789012",
+      cluster: "C",
+      name: "Item 3",
+      quantity: 75,
+      categoryId: "cat3",
+      description: "Description for Item 3",
+      dateOfPurchase: new Date(),
+    },
+    {
+      id: "728ed52h",
+      barcode: 789012,
+      serialNumber: "SN789012",
+      cluster: "C",
+      name: "Item 3",
+      quantity: 75,
+      categoryId: "cat3",
+      description: "Description for Item 3",
+      dateOfPurchase: new Date(),
+    },
   ];
 }
 
 export default async function page() {
   const data = await getData();
   return (
-    <div>
+    <div className="border border-primary-foreground/50 rounded-xl  p-4 gap-4 justify-start items-start w-full">
+      <h1 className="py-2 w-full border-b-[1px] border-b-primary-foreground/50 text-primary-foreground font-bold text-2xl">
+        Inventory
+      </h1>
       <DataTable columns={columns} data={data} />
     </div>
   );
--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -7,11 +7,13 @@ export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-dashboardBackground px-2 py-4">
+    <div className="flex h-screen flex-col md:flex-row overflow-hidden bg-dashboardBackgroundDark px-2 py-4 ">
       <div className="w-full h-full flex-none md:w-64">
         <SideBar />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-2 overflow-y-scroll scrollbar-thin scrollbar-thumb-primary scrollbar-track-dashboardBackground scrollbar-corner-dashboardBackground">
+        {children}
+      </div>
     </div>
   );
 }
